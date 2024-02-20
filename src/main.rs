6cc1a843eaa6fa9613800798710b66c9ebc0a2b6
dbusteed <busteed.davis@gmail.com>
2024-02-20 16:03:47 -0500

    step 3

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use glam::f32::Vec3;
 use sdl2::event::Event;
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels;
 use sdl2::pixels::Color;
 use std::f32::consts::PI;
 
@@ -10,13 +9,27 @@ const WIDTH: usize = 1024;
 const HEIGHT: usize = 768;
 const FOV: f32 = PI / 2.0;
 
+#[derive(Clone, Copy)]
+struct Material {
+    diffuse_color: Vec3,
+}
+
 struct Sphere {
     center: Vec3,
     radius: f32,
+    material: Material,
 }
 
 impl Sphere {
-    fn ray_intersect(&self, orig: Vec3, dir: Vec3, _dist: f32) -> bool {
+    fn new(c: Vec3, r: f32, m: Material) -> Self {
+        Sphere {
+            center: c,
+            radius: r,
+            material: m
+        }
+    }
+
+    fn ray_intersect(&self, orig: Vec3, dir: Vec3, t0: &mut f32) -> bool {
         let big_l: Vec3 = self.center - orig;
         let tca: f32 = big_l.dot(dir);
         let d2: f32 = big_l.dot(big_l) - (tca * tca);
@@ -24,20 +37,38 @@ impl Sphere {
             return false;
         }
         let thc: f32 = f32::sqrt(self.radius*self.radius - d2);
-        let mut t0 = tca - thc;
+        *t0 = tca - thc;
         let t1 = tca + thc;
-        if t0 < 0.0 { t0 = t1 };
-        if t0 < 0.0 { return false; }
+        if *t0 < 0.0 { *t0 = t1 };
+        if *t0 < 0.0 { return false; }
         return true;
     }
 }
 
-fn cast_ray(orig: Vec3, dir: Vec3, sphere: &Sphere) -> Vec3 {
-    let sphere_dist: f32 = f32::MAX;
-    if !sphere.ray_intersect(orig, dir, sphere_dist) {
-        return Vec3::new(0.2, 0.7, 0.8);
+fn scene_intersect(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>, hit: &mut Vec3, big_n: &mut Vec3, material: &mut Material) -> bool {
+    let mut sphere_dist: f32 = f32::MAX;
+    for sphere in spheres.iter() {
+        let mut dist: f32 = 0.0;
+        if sphere.ray_intersect(orig, dir, &mut dist) && dist < sphere_dist {
+            sphere_dist = dist;
+            *hit = orig + dir * dist;
+            *big_n = (*hit - sphere.center).normalize();
+            *material = sphere.material;
+        }
     }
-    return Vec3::new(0.4, 0.4, 0.3);
+    sphere_dist < 1000.0 
+}
+
+fn cast_ray(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>) -> Vec3 {
+    let mut point = Vec3::ZERO;
+    let mut big_n = Vec3::ZERO;
+    let mut material = Material { diffuse_color: Vec3::ZERO };
+
+    if !scene_intersect(orig, dir, &spheres, &mut point, &mut big_n, &mut material) {
+        return Vec3::new(0.2, 0.7, 0.8); // background material 
+    }
+    
+    return material.diffuse_color;
 }
 
 fn render(image: &Vec<Vec3>) -> Result<(), String> {
@@ -86,10 +117,15 @@ fn render(image: &Vec<Vec3>) -> Result<(), String> {
 }
 
 fn main() {
-    let sphere = Sphere {
-        center: Vec3::new(-3.0, 0.0, -16.0),
-        radius: 2.0,
-    };
+    let ivory = Material { diffuse_color: Vec3::new(0.4, 0.4, 0.3) };
+    let red_rubber = Material { diffuse_color: Vec3::new(0.3, 0.1, 0.1) };
+
+    let spheres = vec![
+        Sphere::new(Vec3::new(-3.0,  0.0, -16.0), 2.0, ivory),
+        Sphere::new(Vec3::new(-1.0, -1.5, -12.0), 2.0, red_rubber),
+        Sphere::new(Vec3::new( 1.5, -0.5, -18.0), 3.0, red_rubber),
+        Sphere::new(Vec3::new( 7.0,  5.0, -18.0), 4.0, ivory),
+    ];
 
     let mut image = vec![Vec3::ZERO; WIDTH * HEIGHT];
 
@@ -101,8 +137,9 @@ fn main() {
             x = (2.0 * (col as f32 + 0.5) / (WIDTH as f32) - 1.0) * (FOV / 2.0).tan() * (WIDTH as f32 / HEIGHT as f32);
             y = -(2.0 * (row as f32 + 0.5) / (HEIGHT as f32) - 1.0) * (FOV / 2.0).tan();
             dir = Vec3::new(x, y, -1.0).normalize();
-            image[row * WIDTH + col] = cast_ray(Vec3::ZERO, dir, &sphere);
+            image[row * WIDTH + col] = cast_ray(Vec3::ZERO, dir, &spheres);
         }
     }
+    
     let _ = render(&image);
 }
