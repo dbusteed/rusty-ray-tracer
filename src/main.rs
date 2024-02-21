a5e7d98c0dbd9e166612537c49111aebb06b3c31
dbusteed <busteed.davis@gmail.com>
2024-02-20 21:25:55 -0500

    step 7

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glam::f32::{Vec2, Vec3};
+use glam::f32::Vec3;
 use sdl2::event::Event;
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
@@ -11,13 +11,13 @@ const FOV: f32 = PI / 2.0;
 
 #[derive(Clone, Copy)]
 struct Material {
-    albedo: Vec2,
+    albedo: Vec3,
     diffuse_color: Vec3,
     specular_exponent: f32,
 }
 
 impl Material {
-    fn new(a: Vec2, c: Vec3, s: f32) -> Self {
+    fn new(a: Vec3, c: Vec3, s: f32) -> Self {
         Material {
             albedo: a,
             diffuse_color: c,
@@ -85,14 +85,22 @@ fn norm(v: Vec3) -> f32 {
     f32::sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
 }
 
-fn cast_ray(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>, lights: &Vec<Light>) -> Vec3 {
+fn cast_ray(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>, lights: &Vec<Light>, depth: usize) -> Vec3 {
     let mut point = Vec3::ZERO;
     let mut big_n = Vec3::ZERO;
-    let mut material = Material::new(Vec2::ZERO, Vec3::ZERO, 0.0);
+    let mut material = Material::new(Vec3::ZERO, Vec3::ZERO, 0.0);
 
-    if !scene_intersect(orig, dir, &spheres, &mut point, &mut big_n, &mut material) {
+    if depth > 4 || !scene_intersect(orig, dir, &spheres, &mut point, &mut big_n, &mut material) {
         return Vec3::new(0.2, 0.7, 0.8); // background material 
     }
+
+    let reflect_dir = reflect(dir, big_n).normalize();
+    let reflect_orig = if reflect_dir.dot(big_n) < 0.0 {
+        point - big_n * 0.001
+    } else {
+        point + big_n * 0.001
+    };
+    let reflect_color = cast_ray(reflect_orig, reflect_dir, &spheres, lights, depth + 1);
     
     let mut diffuse_light_intensity: f32 = 0.0;
     let mut specular_light_intensity: f32 = 0.0;
@@ -108,7 +116,7 @@ fn cast_ray(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>, lights: &Vec<Light>) -
         };
         let mut _point = Vec3::ZERO;
         let mut _big_n = Vec3::ZERO;
-        let mut _material = Material::new(Vec2::ZERO, Vec3::ZERO, 0.0);
+        let mut _material = Material::new(Vec3::ZERO, Vec3::ZERO, 0.0);
         if scene_intersect(_orig, light_dir, &spheres, &mut _point, &mut _big_n, &mut _material) && norm(_point - _orig) < light_distance {
             continue;
         }
@@ -116,7 +124,7 @@ fn cast_ray(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>, lights: &Vec<Light>) -
         diffuse_light_intensity += light.intensity * f32::max(0.0, light_dir.dot(big_n));
         specular_light_intensity += f32::powf(f32::max(0.0, -reflect(-light_dir, big_n).dot(dir)), material.specular_exponent) * light.intensity;
     }
-    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3::ONE * specular_light_intensity * material.albedo[1];
+    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3::ONE * specular_light_intensity * material.albedo[1] + reflect_color * material.albedo[2];
 }
 
 fn render(image: &Vec<Vec3>) -> Result<(), String> {
@@ -165,14 +173,15 @@ fn render(image: &Vec<Vec3>) -> Result<(), String> {
 }
 
 fn main() {
-    let ivory = Material::new(Vec2::new(0.6, 0.3), Vec3::new(0.4, 0.4, 0.3), 50.0);
-    let red_rubber = Material::new(Vec2::new(0.9, 0.1), Vec3::new(0.3, 0.1, 0.1), 10.0);
+    let ivory      = Material::new(Vec3::new(0.6,  0.3, 0.1), Vec3::new(0.4, 0.4, 0.3),   50.0);
+    let red_rubber = Material::new(Vec3::new(0.9,  0.1, 0.0), Vec3::new(0.3, 0.1, 0.1),   10.0);
+    let mirror     = Material::new(Vec3::new(0.0, 10.0, 0.8), Vec3::new(1.0, 1.0, 1.0), 1425.0);
 
     let spheres = vec![
         Sphere::new(Vec3::new(-3.0,  0.0, -16.0), 2.0, ivory),
-        Sphere::new(Vec3::new(-1.0, -1.5, -12.0), 2.0, red_rubber),
+        Sphere::new(Vec3::new(-1.0, -1.5, -12.0), 2.0, mirror),
         Sphere::new(Vec3::new( 1.5, -0.5, -18.0), 3.0, red_rubber),
-        Sphere::new(Vec3::new( 7.0,  5.0, -18.0), 4.0, ivory),
+        Sphere::new(Vec3::new( 7.0,  5.0, -18.0), 4.0, mirror),
     ];
 
     let lights = vec![
@@ -191,7 +200,7 @@ fn main() {
             x = (2.0 * (col as f32 + 0.5) / (WIDTH as f32) - 1.0) * (FOV / 2.0).tan() * (WIDTH as f32 / HEIGHT as f32);
             y = -(2.0 * (row as f32 + 0.5) / (HEIGHT as f32) - 1.0) * (FOV / 2.0).tan();
             dir = Vec3::new(x, y, -1.0).normalize();
-            image[row * WIDTH + col] = cast_ray(Vec3::ZERO, dir, &spheres, &lights);
+            image[row * WIDTH + col] = cast_ray(Vec3::ZERO, dir, &spheres, &lights, 0);
         }
     }
     
