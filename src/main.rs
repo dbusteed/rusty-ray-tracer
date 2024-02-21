4f0d8f66f846e37f8c53f11035cd3b4f7f7c6f6c
dbusteed <busteed.davis@gmail.com>
2024-02-20 22:41:23 -0500

    step last

--- src/main.rs
+++ src/main.rs
@@ -112,7 +112,7 @@ fn cast_ray(orig: Vec3, dir: Vec3, spheres: &Vec<Sphere>, lights: &Vec<Light>, d
     let mut material = Material::new(0.0, Vec4::ZERO, Vec3::ZERO, 0.0);
 
     if depth > 4 || !scene_intersect(orig, dir, &spheres, &mut point, &mut big_n, &mut material) {
-        return Vec3::new(0.2, 0.7, 0.8); // background material 
+        return Vec3::new(0.5, 0.8, 0.9); // background material 
     }
 
     let reflect_dir = reflect(dir, big_n).normalize();
@@ -194,22 +194,30 @@ fn render(image: &Vec<Vec3>) -> Result<(), String> {
 }
 
 fn main() {
-    let ivory      = Material::new(1.0, Vec4::new(0.6,  0.3, 0.1, 0.0), Vec3::new(0.4, 0.4, 0.3),   50.0);
-    let glass      = Material::new(1.5, Vec4::new(0.0,  0.5, 0.1, 0.8), Vec3::new(0.6, 0.7, 0.8),  125.0);
-    let red_rubber = Material::new(1.0, Vec4::new(0.9,  0.1, 0.0, 0.0), Vec3::new(0.3, 0.1, 0.1),   10.0);
-    let mirror     = Material::new(1.0, Vec4::new(0.0, 10.0, 0.8, 0.0), Vec3::new(1.0, 1.0, 1.0), 1425.0);
+    let ivory = Material::new(1.0, Vec4::new(0.6,  0.3, 0.1, 0.0), Vec3::new(0.4, 0.4, 0.3),   50.0);
+    let glass = Material::new(1.5, Vec4::new(0.0,  0.5, 0.1, 0.8), Vec3::new(0.6, 0.7, 0.8),  125.0);
+    let red_rubber = Material::new(1.0, Vec4::new(0.9,  0.1, 0.0, 0.0), Vec3::new(0.3, 0.1, 0.1), 10.0);
+    let blue_rubber = Material::new(1.0, Vec4::new(0.8,  0.1, 0.0, 0.0), Vec3::new(0.2, 0.2, 0.6), 10.0);
+    let purp_rubber = Material::new(1.0, Vec4::new(0.8,  0.1, 0.0, 0.0), Vec3::new(0.8, 0.2, 0.8), 10.0);
+    let other_rubber = Material::new(1.0, Vec4::new(0.8,  0.1, 0.0, 0.0), Vec3::new(0.9, 0.9, 0.1), 10.0);
+    let green_rubber = Material::new(1.0, Vec4::new(0.9,  0.1, 0.0, 0.0), Vec3::new(0.1, 0.6, 0.1), 10.0);
+    let _mirror = Material::new(1.0, Vec4::new(0.0, 10.0, 0.8, 0.0), Vec3::new(1.0, 1.0, 1.0), 1425.0);
 
     let spheres = vec![
-        Sphere::new(Vec3::new(-3.0,  0.0, -16.0), 2.0, ivory),
-        Sphere::new(Vec3::new(-1.0, -1.5, -12.0), 2.0, glass),
-        Sphere::new(Vec3::new( 1.5, -0.5, -18.0), 3.0, red_rubber),
-        Sphere::new(Vec3::new( 7.0,  5.0, -18.0), 4.0, mirror),
+        Sphere::new(Vec3::new(-3.0,  1.5, -16.0), 2.0, ivory),
+        Sphere::new(Vec3::new(-1.0, -1.0, -12.0), 2.0, glass),
+        Sphere::new(Vec3::new( 1.5, 1.0, -18.0), 3.0, red_rubber),
+        Sphere::new(Vec3::new(8.0,  0.5, -16.0), 4.0, glass),
+        Sphere::new(Vec3::new(7.0, 4.5, -25.0), 3.0, other_rubber),
+        Sphere::new(Vec3::new(-10.0, -9.5, -30.0), 4.0, green_rubber),
+        Sphere::new(Vec3::new(8.0, -8.5, -20.0), 4.0, blue_rubber),
+        Sphere::new(Vec3::new(0.0, -6.5, -22.0), 3.0, purp_rubber),
     ];
 
     let lights = vec![
         Light { position: Vec3::new(-20.0, 20.0,  20.0), intensity: 1.5 },
-        Light { position: Vec3::new( 30.0, 50.0, -25.0), intensity: 1.8 },
-        Light { position: Vec3::new( 30.0, 20.0,  30.0), intensity: 1.7 },
+        Light { position: Vec3::new( 30.0, 50.0, -25.0), intensity: 1.5 },
+        Light { position: Vec3::new( 30.0, 20.0,  30.0), intensity: 1.5 },
     ];
 
     let mut image = vec![Vec3::ZERO; WIDTH * HEIGHT];
